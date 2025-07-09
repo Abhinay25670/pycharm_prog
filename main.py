13fe3dfabf95fe4d5720ca6a28b0a7afe80b8eed
Abhinay25670 <224g1a3202@srit.ac.in>
2025-07-09 09:09:04 +0000

    new file

---
+++ main.py
@@ -0,0 +1,4 @@
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+print(f"Hello,{name}! you will be {age+5} years old in 5years.")
+
---
+++ numbers.py
@@ -0,0 +1,11 @@
+num=5
+arr=[]
+sum=0
+avg=0
+for i in range(0,num):
+    arr.append(int(input()))
+    sum+=num
+avg=sum/num
+print(arr)
+print(sum)
+print(avg)
